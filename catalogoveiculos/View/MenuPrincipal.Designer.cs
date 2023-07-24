42e8a7662383831edf8b741227697272ae419d99
PAMELA THAIS GOMES DA PAZ <pamelagomes18@hotmail.com>
2023-07-24 09:55:41 -0300

    TelaMenuPrincipal

--- catalogoveiculos/View/MenuPrincipal.Designer.cs
+++ catalogoveiculos/View/MenuPrincipal.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.placaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.corToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.códigoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.marcaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alterarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deletarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.carrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // carrosToolStripMenuItem
+            // 
+            this.carrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarToolStripMenuItem,
+            this.buscarToolStripMenuItem,
+            this.alterarToolStripMenuItem,
+            this.deletarToolStripMenuItem});
+            this.carrosToolStripMenuItem.Name = "carrosToolStripMenuItem";
+            this.carrosToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.carrosToolStripMenuItem.Text = "Veículos";
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cadastrarToolStripMenuItem.Text = "Cadastrar";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            this.buscarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.códigoToolStripMenuItem,
+            this.nomeToolStripMenuItem,
+            this.marcaToolStripMenuItem,
+            this.placaToolStripMenuItem,
+            this.anoToolStripMenuItem,
+            this.modeloToolStripMenuItem,
+            this.corToolStripMenuItem});
+            this.buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            this.buscarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.buscarToolStripMenuItem.Text = "Buscar";
+            // 
+            // anoToolStripMenuItem
+            // 
+            this.anoToolStripMenuItem.Name = "anoToolStripMenuItem";
+            this.anoToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.anoToolStripMenuItem.Text = "Ano";
+            // 
+            // placaToolStripMenuItem
+            // 
+            this.placaToolStripMenuItem.Name = "placaToolStripMenuItem";
+            this.placaToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.placaToolStripMenuItem.Text = "Placa";
+            // 
+            // corToolStripMenuItem
+            // 
+            this.corToolStripMenuItem.Name = "corToolStripMenuItem";
+            this.corToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.corToolStripMenuItem.Text = "Cor";
+            // 
+            // nomeToolStripMenuItem
+            // 
+            this.nomeToolStripMenuItem.Name = "nomeToolStripMenuItem";
+            this.nomeToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.nomeToolStripMenuItem.Text = "Nome";
+            // 
+            // códigoToolStripMenuItem
+            // 
+            this.códigoToolStripMenuItem.Name = "códigoToolStripMenuItem";
+            this.códigoToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.códigoToolStripMenuItem.Text = "Código";
+            // 
+            // marcaToolStripMenuItem
+            // 
+            this.marcaToolStripMenuItem.Name = "marcaToolStripMenuItem";
+            this.marcaToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.marcaToolStripMenuItem.Text = "Marca";
+            // 
+            // alterarToolStripMenuItem
+            // 
+            this.alterarToolStripMenuItem.Name = "alterarToolStripMenuItem";
+            this.alterarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.alterarToolStripMenuItem.Text = "Alterar";
+            // 
+            // deletarToolStripMenuItem
+            // 
+            this.deletarToolStripMenuItem.Name = "deletarToolStripMenuItem";
+            this.deletarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deletarToolStripMenuItem.Text = "Deletar";
+            // 
+            // modeloToolStripMenuItem
+            // 
+            this.modeloToolStripMenuItem.Name = "modeloToolStripMenuItem";
+            this.modeloToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.modeloToolStripMenuItem.Text = "Modelo";
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(610, 372);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MenuPrincipal";
+            this.ShowIcon = false;
             this.Text = "MenuPrincipal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem arquivoToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+        private ToolStripMenuItem carrosToolStripMenuItem;
+        private ToolStripMenuItem cadastrarToolStripMenuItem;
+        private ToolStripMenuItem buscarToolStripMenuItem;
+        private ToolStripMenuItem códigoToolStripMenuItem;
+        private ToolStripMenuItem nomeToolStripMenuItem;
+        private ToolStripMenuItem marcaToolStripMenuItem;
+        private ToolStripMenuItem placaToolStripMenuItem;
+        private ToolStripMenuItem anoToolStripMenuItem;
+        private ToolStripMenuItem modeloToolStripMenuItem;
+        private ToolStripMenuItem corToolStripMenuItem;
+        private ToolStripMenuItem alterarToolStripMenuItem;
+        private ToolStripMenuItem deletarToolStripMenuItem;
     }
 }
