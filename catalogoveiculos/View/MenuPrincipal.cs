93e98d46990fcf12a48fd5766720205d75b8462c
PAMELA THAIS GOMES DA PAZ <pamelagomes18@hotmail.com>
2023-07-24 09:44:55 -0300

    TelaMenuPrincipal

--- catalogoveiculos/View/MenuPrincipal.cs
+++ catalogoveiculos/View/MenuPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace catalogoveiculos.View
 {
-    public partial class Form1 : Form
+    public partial class MenuPrincipal : Form
     {
-        public Form1()
+        public MenuPrincipal()
         {
             InitializeComponent();
         }
